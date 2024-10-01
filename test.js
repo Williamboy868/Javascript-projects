c9844314cdf96e1ca3b4d71c6c3518ddf4f0971f
williamboy868 <kingofintelligence8@gmail.com>
2024-10-01 20:50:22 +0000

    first commit

---
+++ test.js
@@ -0,0 +1,2 @@
+const zaptekApplicant = {"firstName":"Williams", "secondName":"Adusei", "school":"KNUST", "course":"mobile development", "fullName":function(){return this.firstName + " "+ this.secondName}};
+console.log(zaptekApplicant.fullName)
