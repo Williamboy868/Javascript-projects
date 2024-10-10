ff3419b70adc4376d5e69ac38c759c49648810d7
williamboy868 <kingofintelligence8@gmail.com>
2024-10-10 04:22:49 +0000

    New changes

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Text, View, Image} from 'react-native';
+import {Text, View, Image, SafeAreaView} from 'react-native';
 
 const name = "Williams Adusei";
 const school = "Kwame Nkrumah University of Science and Technology(KNUST)";
@@ -7,20 +7,23 @@ const skills = "I'm a tech enthusiast with a fair knowledge of C++,C#,Python and
 
 const Portfolio = () => {
   return (
-    <View>
-      <Image
-        source={{
-          uri: "C:\Users\HP\OneDrive\Documents\OneNote Notebooks\blur.png",
-        }}
-        style={{width: 200, height: 200}}
-      />
-      <Text
-      style={{fontStyle:'italic', fontColor:'red'}}>Name:{name}</Text>
-      <Text
-      style={{fontStyle:'italic', fontColor:'red'}}>School:{school}</Text>
-      <Text
-      style={{fontStyle:'italic', fontColor:'red'}}>skills and Interests:{skills}</Text>
-    </View>
+    <SafeAreaView
+    style ={{flex:1}}>
+      <View>
+        <Image
+          source={
+          require('../Work-App/assets/blur.png')
+          }
+          style={{width: 200, height: 200}}
+        />
+        <Text
+        style={{fontStyle:'italic', fontColor:'red'}}>Name:{name}</Text>
+        <Text
+        style={{fontStyle:'italic', fontColor:'red'}}>School:{school}</Text>
+        <Text
+        style={{fontStyle:'italic', fontColor:'red'}}>skills and Interests:{skills}</Text>
+        </View>
+    </SafeAreaView>
   );
 };
 
